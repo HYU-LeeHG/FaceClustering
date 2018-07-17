203dec4f105bcf04f65cf75ace36bc2757754dcf
HYU-LeeHG <qnd4321@naver.com>
2018-07-17 19:54:02 +0900

    1_img modify

--- 1_Text_recognition/text_recognition.py
+++ 1_Text_recognition/text_recognition.py
@@ -2,11 +2,23 @@ import sys
 from pytesseract import image_to_string
 from PIL import Image
 import cv2 as cv
+import numpy as np
 
 
 
 def get_image(filename):
-    return Image.open(filename)
+    img = cv.imread(filename, cv.IMREAD_COLOR)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(img,(3,3),0)
+    cv.imwrite('blur.jpg',blur)
+    canny = cv.Canny(blur, 100, 200)
+    cv.imwrite('canny.jpg',canny)
+
+    return img
+
+
+
+    #return Image.open(filename)
     #return cv.imread(filename)
 
 if __name__ == '__main__' :
